88307ddc65263e019848a6d2230f283b040ec773
Orbit User <Orbit User@DESKTOP-CO1C19B>
2018-01-12 20:26:24 +0100

    Actualzar bootstrap 4.0.0  y añadir sistema de navegación. Añadir vistas de partida, informacion y listado Hacer el navbar

--- RandomRol/www/scripts/index.js
+++ RandomRol/www/scripts/index.js
@@ -1,30 +1,126 @@
-﻿// Si quiere una introducción sobre la plantilla En blanco, vea la siguiente documentación:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// Para depurar código al cargar la página en cordova-simulate o en dispositivos o emuladores Android: inicie la aplicación, establezca puntos de interrupción 
-// y ejecute "window.location.reload()" en la Consola de JavaScript.
-(function () {
-    "use strict";
+﻿var app = {
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    // inicializa la aplicacion cuando el dispositivo está preparado (ondeviceready)
+    init: function () {
+        this.historial = [];
+        this.titulo = $('titulo');      // reference to the title bar
+        this.volver = $('volver');      // reference to the back button
+
+        this.volver.addEventListener('click', function (e) {
+            this.navega('vuelve', 'left');
+        }.bind(this));
+
+        this.navega('menu.html');
+    },
+
+    // Enrutador de navegacion
+    navega: function (url, direccion) {
+
+        // add this view to the history stack
+        url = this.historiza(url);
+
+        // if the history stack is empty, hide the back button
+        this.volver.style.opacity = (this.historial.length > 1) ? 1 : 0;
+
+        // load new views with an XHR request
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = function (e) {
+            var response = e.currentTarget;
+            if (response.readyState !== 4) return;
+            if (response.status !== 200 && response.status !== 0) {
+                console.error('error al cargar la url: ', response.status, response.responseText);
+            }
+
+            // crear vista y publicarla
+            var el = document.createElement('div');
+            el.className = 'aplicacion';
+            el.innerHTML = response.responseText;
+
+            this.hidrata(el);                   // crear enlaces de enrutador y cargar scripts
+            this.entra(el, direccion);          // anima la entrada en la nueva vista
+            this.sale(this.page, direccion);    // anima la salida de la anterior vista
+            this.page = el;                     // create a reference to the active view
+
+        }.bind(this);
+        xhr.send();
+    },
+
+    // anima una nueva vista en la pantalla
+    entra: function (el, direccion) {
+        if (!el) return; // paranoia
+
+        // cargar la nueva vista fuera de la pantalla
+        var x = document.body.offsetWidth;
+        if (direccion == 'left') x = -x;
+        el.style.transform = 'translateX(' + x + 'px)';
+        document.body.appendChild(el);
+
+        // después de un breve retraso para permitir pintar el DOM, anima la vista en la pantalla
+        window.setTimeout(function () {
+            el.style.transform = 'translateX(0)';
+        }, 50);
+
+    },
+
+    // crear enlaces de enrutador y cargar scripts
+    hidrata: function (el) {
+        var hrefs = el.querySelectorAll('*[data-href]');
+        for (var i = 0; i < hrefs.length; i++) {
+            hrefs[i].addEventListener('click', this.navega.bind(this, hrefs[i].getAttribute('data-href')), false);
+        }
+
+        var scripts = el.querySelectorAll('*[data-script]');
+        for (var i = 0; i < scripts.length; i++) {
+            this.require(scripts[i].getAttribute('data-script'));
+        }
+
+        var titulo = el.querySelectorAll('.titulo');
+        if (titulo.length > 0) {
+            this.titulo.innerHTML = '';
+            this.titulo.appendChild(titulo[0]);
+        }
+    },
+
+    // animate an outgoing view off of the screen
+    sale: function (el, direccion) {
+        if (!el) return; // paranoia
+
+        // encuentra lo que constituye "fuera de pantalla"
+        var x = -document.body.offsetWidth;
+        if (direccion == 'left') x = -x;
         
-    function onDeviceReady() {
-        // Controlar la pausa de Cordova y reanudar eventos
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova se ha cargado. Haga aquí las inicializaciones que necesiten Cordova.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-    };
+        // después de un breve retraso para permitir el dibujado del DOM, anima la vista anterior fuera de la pantalla
+        window.setTimeout(function () {
+            el.style.transform = 'translateX(' + x + 'px)';
+        }, 50);
 
-    function onPause() {
-        // TODO: esta aplicación se ha suspendido. Guarde el estado de la aplicación aquí.
-    };
+        // eliminar el elemento después de que la transición haya terminado
+        window.setTimeout(function () {
+            el.parentElement.removeChild(el);
+        }, 1000);
 
-    function onResume() {
-        // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
-    };
-})();
+    },
+
+    // administra la pila del historial
+    historiza: function (url) {
+        if (url == 'vuelve') {
+            this.historial.pop();
+            url = this.historial[this.historial.length - 1];
+        } else {
+            this.historial.push(url);
+        }
+        return url;
+    }
+}
+
+// funciones de conveniencia
+function $(str) {
+    return document.getElementById(str);
+}
+
+function $$(str) {
+    return document.querySelectorAll(str);
+}
+
+document.addEventListener('deviceready', function (e) { app.init(); }, false);
